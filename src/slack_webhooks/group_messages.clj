071d71a3620e17d602c503d709fcaa7ce7095c81
Carter Halamka <chalamka@gmail.com>
2016-03-14 12:07:33 -0500

    Additional work on group messsages

--- src/slack_webhooks/group_messages.clj
+++ src/slack_webhooks/group_messages.clj
@@ -7,8 +7,8 @@
 
 (defn group-exists?
   "return whether or not a group exists"
-  [group all-groups]
-  (if (get @all-groups (keyword group))
+  [group all-groups privileged-groups]
+  (if (get (merge @all-groups @privileged-groups) (keyword group))
     true
     false))
 
@@ -19,30 +19,49 @@
 
 (defn user-in-group?
   "return whether or not a user is a member of a group"
-  [user group all-groups]
+  [user group all-groups privileged-groups]
   (let [group-key (keyword group)]
-    (in? (-> @all-groups group-key) user)))
+    (in? (-> (merge @all-groups @privileged-groups)  group-key) user)))
 
 (defn add-user "add a user or users to a group" [sender group all-groups privileged-groups & user]
-  (let [exists (group-exists? group all-groups)
+  (let [exists (group-exists? group all-groups privileged-groups)
         privileged (privileged-sender? sender privileged-groups)
         group-key (keyword group)]
     (if exists
       (cond
         (and privileged (in? (keys @privileged-groups) group-key))
-          (swap! privileged-groups assoc group-key (conj (group-key @privileged-groups) user))
+          (swap! privileged-groups assoc group-key (into (group-key @privileged-groups) user))
+        (and (not privileged) (in? (keys @privileged-groups) group-key))
+          (str sender " is not privileged")
         :default-cond
-          (swap! all-groups assoc group-key (conj (group-key @all-groups) user)))
+          (swap! all-groups assoc group-key (into (group-key @all-groups) user)))
       (str "No such group: " group))))
 
 (defn add-group "add a group with sender as member" [sender group all-groups]
   (swap! all-groups assoc (keyword group) #{sender}))
 
-(defn raise-group-permission "raise a group to privileged status" [sender group])
+(defn raise-group-permission "raise a group to privileged status" [sender group all-groups privileged-groups]
+  (let [exists (group-exists? group all-groups (atom {}))
+        privileged (privileged-sender? sender privileged-groups)
+        group-key (keyword group)]
+    (if (and exists privileged)
+      (let [group-members (group-key @all-groups)]
+        (swap! all-groups dissoc group-key)
+        (swap! privileged-groups assoc group-key group-members))
+      (str "No such group: " group " or " sender " not privileged"))))
 
-(defn lower-group-permission "lower a group to standard status" [sender group])
+(defn lower-group-permission "lower a group to standard status" [sender group all-groups privileged-groups]
+  (let [exists (group-exists? group (atom {}) privileged-groups)
+        privileged (privileged-sender? sender privileged-groups)
+        group-key (keyword group)]
+    (if (and exists privileged (< 1 (count @privileged-groups)))
+      (do
+        (swap! all-groups assoc group-key (group-key @privileged-groups))
+        (swap! privileged-groups dissoc group-key))
+      (str "Some sort of failure"))))
 
-(defn remove-user "remove a user or users from a group" [sender group & user])
+(defn remove-user "remove a user or users from a group" [sender group all-groups privileged-groups & user]
+  )
 
 (defn remove-group "remove a group" [sender group])
 
@@ -51,17 +70,17 @@
 (defn get-group-members
   "get all the members of a group"
   [sender group all-groups privileged-groups]
-  (let [exists (group-exists? group all-groups)
+  (let [exists (group-exists? group all-groups privileged-groups)
         privileged (privileged-sender? sender privileged-groups)
         group-key (keyword group)]
     (cond (and exists privileged)
-            (-> @all-groups group-key printable-seq)
+            (-> (merge @all-groups @privileged-groups) group-key printable-seq)
           (true? exists)
-            "Not privileged"
+            (str sender " is not privileged")
           (true? privileged)
-            "No such group"
+            (str "No such group: " group)
           :default-cond
-            "Not privileged and no such group")))
+            (str sender " is not privileged and no such group: " group) )))
 
-(defn get-member-groups "get groups for sender, user, or users" [sender all-groups & users]
-  (map key (filter #(-> % val (contains? sender)) @all-groups)))
+(defn get-member-groups "get groups for sender, user, or users" [sender all-groups privileged-groups & users]
+  (map key (filter #(-> % val (contains? sender)) (merge @all-groups @privileged-groups) )))
--- test/slack_webhooks/group_messages_test.clj
+++ test/slack_webhooks/group_messages_test.clj
@@ -10,7 +10,7 @@
 
 (deftest test-group-exists?
   (testing "works as expected"
-    (is (= (group-exists? "test1" test-all-groups-atom) true))))
+    (is (= (group-exists? "test1" test-all-groups-atom test-privileged-groups-atom) true))))
 
 (deftest test-privileged-sender?
   (testing "works as expected"
@@ -18,28 +18,64 @@
 
 (deftest test-user-in-group?
   (testing "returns true if user is in group"
-    (is (true? (user-in-group? "ledif" "test3" test-all-groups-atom))))
+    (is (true? (user-in-group? "ledif" "test3" test-all-groups-atom test-privileged-groups-atom))))
   (testing "returns false if user is not in group"
-    (is (nil? (user-in-group? "notLedif" "test3" test-all-groups-atom)))))
+    (is (nil? (user-in-group? "notLedif" "test3" test-all-groups-atom test-privileged-groups-atom)))))
 
-(deftest test-validation
-  (testing "work as expected"
-    (let [pred "test"
-          coll '(#{"test" "stuff"} #{"more" "things"})]
-      (is (some true? (map #(contains? % pred) coll))))))
+(deftest test-add-user
+  (let [groups (atom @test-all-groups-atom)
+        priv (atom @test-privileged-groups-atom)
+        new-dict {:test1 #{"testmember1" "2" "3" "4"}
+                  :test2 #{"testmember1" "2"}
+                  :test3 #{"ledif" "new-user"}}
+        new-priv-dict {:admins #{"ledif" "not-ledif"}}]
+    (testing "adds a user to a group if it exists"
+      (is (= new-dict (add-user "ledif" "test3" groups priv "new-user"))))
+    (testing "adds a user to privileged groups"
+      (is (= new-priv-dict (add-user "ledif" "admins" groups priv "not-ledif"))))))
 
 (deftest test-add-group
   (testing "adds a group to atom with sender as member"
-    (let [new-dict {:test1 #{"testmember1" "2" "3" "4"}
+    (let [groups (atom @test-all-groups-atom)
+          new-dict {:test1 #{"testmember1" "2" "3" "4"}
                     :test2 #{"testmember1" "2"}
                     :test3 #{"ledif"}
                     :new-group #{"ledif"}}]
-      (is (= new-dict (add-group "ledif" "new-group" test-all-groups-atom))))))
+      (is (= new-dict (add-group "ledif" "new-group" groups))))))
+
+(deftest test-raise-group-permission
+    (let [groups (atom @test-all-groups-atom)
+          priv (atom @test-privileged-groups-atom)
+          new-groups (atom {:test1 #{"testmember1" "2" "3" "4"}
+                            :test2 #{"testmember1" "2"}})
+          new-priv (atom {:admins #{"ledif"}
+                          :test3 #{"ledif"}})]
+      (raise-group-permission "ledif" "test3" groups priv)
+      (testing "raises a group to privileged"
+        (is (true? (and (= @groups @new-groups) (= @priv @new-priv)))))
+      (testing "fails if sender lacks privilege"
+        (is (= "No such group fail or fail-user not privileged") (raise-group-permission "fail-user" "fail" groups priv)))))
+
+(deftest test-lower-group-permission
+  (let [groups (atom @test-all-groups-atom)
+        priv (atom {:admins #{"ledif"}
+                    :other #{"ledif"}})
+        new-groups (atom {:test1 #{"testmember1" "2" "3" "4"}
+                          :test2 #{"testmember1" "2"}
+                          :test3 #{"ledif"}
+                          :other #{"ledif"}})
+        new-priv (atom @test-privileged-groups-atom)]
+    (testing "lowers a group to unprivileged"
+      (lower-group-permission "ledif" "other" groups priv)
+      (is (= @new-priv @priv))
+      (is (= @new-groups @groups)))))
 
 (deftest test-get-group-members
   (testing "returns group members"
     (is (= "ledif" (get-group-members "ledif" "test3" test-all-groups-atom test-privileged-groups-atom))))
+  (testing "returns members of privileged groups"
+    (is (= "ledif" (get-group-members "ledif" "admins" test-all-groups-atom test-privileged-groups-atom))))
   (testing "fails if sender is not privileged"
-    (is (= "Not privileged" (get-group-members "testmember1" "test1" test-all-groups-atom test-privileged-groups-atom))))
+    (is (= "testmember1 is not privileged" (get-group-members "testmember1" "test1" test-all-groups-atom test-privileged-groups-atom))))
   (testing "fails if group does not exist"
-    (is (= "No such group" (get-group-members "ledif" "fail" test-all-groups-atom test-privileged-groups-atom)))))
+    (is (= "No such group: fail" (get-group-members "ledif" "fail" test-all-groups-atom test-privileged-groups-atom)))))
